3865c35298caf5abd86d763132a4d23adc9d0bb9
Silver1793 <git>
2023-10-12 20:54:38 -0400

    feat: added how to connect to mongodb locally

--- app.py
+++ app.py
@@ -6,31 +6,59 @@ import pymongo
 import datetime
 from bson.objectid import ObjectId
 import sys
+from pymongo import MongoClient
 
 app = Flask(__name__)
+# How to connect using a local mongodb
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client['hw05']
+# collection = db['reviews']
+# res = collection.count_documents({})
+# documents = collection.find({})
+
+# # print(res)
+
+# for doc in documents:
+#     print(doc)
+
+# try:
+
+#     if db:
+#         print("Pineppla")
+#     else:
+#         print("HEEE")
+
+# except Exception:
+#     print("HERE")
 
 load_dotenv()  # take environment variables from .env.
 
 # turn on debugging if in development mode
 if os.getenv('FLASK_ENV', 'development') == 'development':
     # turn on debugging, if in development
-    app.debug = True # debug mnode
+    app.debug = True  # debug mnode
 
 # connect to the database
-cxn = pymongo.MongoClient(os.getenv('MONGO_URI'), serverSelectionTimeoutMS=5000)
+cxn = pymongo.MongoClient(os.getenv('MONGO_URI'),
+                          serverSelectionTimeoutMS=5000)
+
 try:
     # verify the connection works by pinging the database
-    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
-    db = cxn[os.getenv('MONGO_DBNAME')] # store a reference to the database
-    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+    # The ping command is cheap and does not require auth.
+    cxn.admin.command('ping')
+    db = cxn[os.getenv('MONGO_DBNAME')]  # store a reference to the database
+    # if we get here, the connection worked!
+    print(' *', 'Connected to MongoDB!')
 except Exception as e:
     # the ping command failed, so the connection is not available.
     print(' *', "Failed to connect to MongoDB at", os.getenv('MONGO_URI'))
-    print('Database connection error:', e) # debug
+    print('Database connection error:', e)  # debug
+
 
 @app.route('/')
 def home():
-    return 'Hello, World!' #Change this to render_template once we build the CSS HTML
+    return 'Hello, World!'  # Change this to render_template once we build the CSS HTML
+
 
 if __name__ == "__main__":
     app.run(debug=True)
