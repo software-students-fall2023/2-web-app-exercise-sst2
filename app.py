79d3ad5b12fcee96f8421b05fa55ed73ebe12c98
Silver1793 <git>
2023-10-13 14:12:33 -0400

    feat: Created new file to connect to the database to move out of app.py

--- app.py
+++ app.py
@@ -1,49 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
-from dotenv import load_dotenv
-import os
 
 import pymongo
-import datetime
 from bson.objectid import ObjectId
-import sys
 from pymongo import MongoClient
 
+from db import db # we import from the db file now
+
 app = Flask(__name__)
-# How to connect using a local mongodb
-# client = MongoClient('mongodb://localhost:27017/')
-# db = client['hw05']
-# collection = db['reviews']
-# res = collection.count_documents({})
-# documents = collection.find({})
 
-# # print(res)
-
-# for doc in documents:
-#     print(doc)
-
-load_dotenv()  # take environment variables from .env.
-
-# turn on debugging if in development mode
-if os.getenv('FLASK_ENV', 'development') == 'development':
-    # turn on debugging, if in development
-    app.debug = True  # debug mnode
-
-# connect to the database
-cxn = pymongo.MongoClient(os.getenv('MONGO_URI'),
-                          serverSelectionTimeoutMS=5000)
-
-try:
-    # verify the connection works by pinging the database
-    # The ping command is cheap and does not require auth.
-    cxn.admin.command('ping')
-    db = cxn[os.getenv('MONGO_DBNAME')]  # store a reference to the database
-    # if we get here, the connection worked!
-    print(' *', 'Connected to MongoDB!')
-except Exception as e:
-    # the ping command failed, so the connection is not available.
-    print(' *', "Failed to connect to MongoDB at", os.getenv('MONGO_URI'))
-    print('Database connection error:', e)  # debug
-
+collection = db['users'] #how to create the collection variable
 
 @app.route('/')
 def home():
@@ -51,4 +16,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
