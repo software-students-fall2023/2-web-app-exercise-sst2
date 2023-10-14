da63fc2fb895e64e1b77649b7c69c178a888d347
Capksz <emosker@gmail.com>
2023-10-13 22:16:30 -0400

    Created authentication pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response, session
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +9,7 @@ import sys
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 # How to connect using a local mongodb
 # client = MongoClient('mongodb://localhost:27017/')
 # db = client['hw05']
@@ -50,7 +51,52 @@ postsCollection = db['posts']
 
 @app.route('/')
 def home():
-    return 'Hello, World!'  # Change this to render_template once we build the CSS HTML
+    return render_template('home.html')  # Change this to render_template once we build the CSS HTML
+
+@app.route('/login', methods=['GET'])
+def loginPage():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def loginProcess():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    user = usersCollection.find_one({'username': username})
+    if user and user['password'] == password:
+        session['username'] = user['username']
+        return redirect(url_for('home'))
+    else:
+        error = "Invalid password or username"
+        return render_template('login.html',error=error)
+    
+@app.route('/signUp', methods=['GET'])
+def signUpPage():
+    return render_template('signup.html')
+
+@app.route('/signUp',methods=['POST'])
+def signUpProcess():
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    confirmPassword = request.form.get('confirmPassword')
+
+    if password != confirmPassword:
+        error = "Passwords do not match"
+        return render_template('signup.html', error = error)
+    
+    user = usersCollection.find_one({'username': username})
+
+    if user:
+        error = "This username already exists"
+        return render_template('signup.html', error = error)
+    newUser = {"username":username, "password":password, "email":email}
+    try:
+        usersCollection.insert_one(newUser)
+        return redirect(url_for('loginPage', signup="success"))
+    except:
+        error = "Something went wrong with creating your account. Please try again..."
+        return render_template('signup.html', error = error)
+
 
 
 if __name__ == "__main__":
